bb899be218cafa4e7b8b5ed3d7e1a86a9ccb2e9c
Aman Kumar Shah <amankumarshah8102@gmail.com>
2024-12-30 09:23:09 +0530

    Working on state

--- Vending_Machine/main.java
+++ Vending_Machine/main.java
@@ -1,13 +1,9 @@
 package Vending_Machine;
 
 public class main {
-    
-    public static void main(String args[]){
-        try {
-            System.out.println("");
-            
-        } catch (Exception e) {
-            
-        }
+
+    public static void main(String args[]) {
+        vendingMachine vendingMachine = new vendingMachine();
+        vendingMachine();
     }
 }
