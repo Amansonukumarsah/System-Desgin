385009d9eedeb1a5d0d2583369f40ce06284a8a3
Aman Kumar Shah <amankumarshah8102@gmail.com>
2025-01-05 22:53:14 +0530

    Rearrange the file and state

--- Vending_Machine/imp/inventory/Stock.java
+++ Vending_Machine/imp/inventory/Stock.java
@@ -1,23 +1,61 @@
 package Vending_Machine.imp.inventory;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Stock {
 
-    private Map<productType, Integer> stockProduct;
+    public static class pair {
+
+        private Integer item;
+        private Integer price;
+
+        pair(Integer item, Integer price) {
+            this.item = item;
+            this.price = price;
+        }
+
+        public void setPrice(int price) {
+            this.price = price;
+        }
+
+        public void setItem(int item) {
+            this.item = item;
+        }
+
+        public Integer getPrice() {
+            return this.price;
+        }
+
+        public Integer getItem() {
+            return this.item;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + item + ", " + price + ")";
+        }
+
+    }
+
+    private Map<productType, List<pair>> stockProduct;
 
     public Stock() {
 
         stockProduct = new HashMap<>();
 
         for (productType productType : productType.values()) {
-            stockProduct.put(productType, 0);
+            Map<Integer, Integer> priceItem = new HashMap<>();
+            priceItem.put(0, 0);
+            stockProduct.put(productType, priceItem);
         }
     }
 
     public Integer getProduct(productType productName) {
-        return stockProduct.getOrDefault(stockProduct, 0);
+        Map<Integer, Integer> priceItem = stockProduct.get(productName);
+        if (priceItem)
+            return stockProduct.getOrDefault(stockProduct, 0);
     }
 
     public void addProduct(productType productName, int quantity) {
--- Vending_Machine/vendingMachine.java
+++ Vending_Machine/vendingMachine.java
@@ -1,5 +1,23 @@
 package Vending_Machine;
 
+import Vending_Machine.vendingState.idelState;
+
 public class vendingMachine {
+
+   private state state;
+   private double balance;
+
+   vendingMachine() {
+      this.state = new idelState();
+      this.balance = 0.0;
+   }
+
+   public void setState(state state) {
+      this.state = state;
+   }
+
+   public void addAmount(double amount) {
+      this.balance = amount;
+   }
 
 }
